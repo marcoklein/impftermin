06df29b31f69c43e05ea87212761cf6742d036b1
BennyFischer <benjamin.fischer@outlook.com>
2021-05-06 21:33:43 +0200

    fix: add leading zero when minute is lower than 10 (#11)

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ debug("Launching Impftermin");
       date.getTime() + configuration.intervalInMinutes * 60000
     );
 
-    return `${nextDate.getHours()}:${nextDate.getMinutes()}`;
+    return `${nextDate.getHours()}:${(nextDate.getMinutes()<10?'0':'') + nextDate.getMinutes()}`;
   };
 
   const runChecks = async () => {
